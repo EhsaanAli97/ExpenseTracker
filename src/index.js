5537fe5abd1b35c77026dc8b527c3f90505e0595
Ehsaan Ali <38082320+EhsaanAli97@users.noreply.github.com>
2021-08-08 00:02:42 +0100

    Expense Tracker

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { SpeechProvider } from '@speechly/react-client';
+
+import { Provider } from './context/context';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<SpeechProvider appId="12a3ad7c-ea70-4c43-b2b3-d8774c3ee294" language="en-US">
+		<Provider>
+			<App />
+		</Provider>
+	</SpeechProvider>,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
